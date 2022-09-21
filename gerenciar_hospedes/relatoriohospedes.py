9894dd0d8de4b129efda0ab0f9ce7e68fe364dac
Thiago Augusto França <thiagoaugusto.franca@hotmail.com>
2022-09-20 22:06:19 -0300

    Enviando para o git

---
+++ gerenciar_hospedes/relatoriohospedes.py
@@ -0,0 +1,14 @@
+Dados = {"nome": 'Thiago', "cpf": '05334405992', "telefone": '47999753438'}
+hospedes = [Dados]
+
+while True:
+    opcao = input()
+    if opcao:
+        pass
+
+    # if opcao == "2":
+    #     if len in hospedes == 0:
+    #         print("Não há hospedes cadastrados.")
+    #     else:
+    #         for i in hospedes:
+    #             # print(f"{i[]} - {} - {}")
