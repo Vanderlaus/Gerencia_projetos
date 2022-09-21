8b66a585b8ffabfe94219bf348419dadab0df81e
Heiss Dieter <weboss@gmail.com>
2022-09-21 18:44:01 -0300

    Envio Branch Heiss

--- gerenciar_hospedes/dieter.py
+++ gerenciar_hospedes/dieter.py
@@ -5,28 +5,36 @@ e perguntar se deseja cadastrar outra pessoa, caso não, voltar ao menu.'''
 
 hospedes = []
 dados = {"nome": '', "cpf": '', "telefone": ''}
-
+continuar = '1'
 while True:
-    varnome = input('Qual o seu nome?')
 
-    while True: 
-        varcpf = input('Qual o seu CPF?')
-        if not(cpf.isnumeric()):
-            print('Isso não é numero.')
-        else:
-            break
-    while True:
-        vartelefone = input('Qual o seu telefone?')
-        if not(telefone.isnumeric()):
-            print('Isso não é numero.')
-        else:
-            break
-    print('Fim de cadastro')
-    dados = {
-    'titulo': varnome, 
-    'editora': varcpf,
-    'autor': vartelefone
-    }
-    dados.append(hospedes)
-    print(hospedes)
-    break
+    if continuar == '1':
+        varnome = input('Qual o seu nome? ')
+        while True: 
+            varcpf = input('Qual o seu CPF? ')
+            if not(varcpf.isnumeric()):
+                print('Isso não é numero.')
+            else:
+                break
+        while True:
+            vartelefone = input('Qual o seu telefone? ')
+            if not(vartelefone.isnumeric()):
+                print('Isso não é numero.')
+            else:
+                break
+        print('Fim de cadastro')
+        dados = {
+            'Nome ': varnome, 
+            'CPF ': varcpf,
+            'Telefone: ': vartelefone
+        }
+        hospedes.append(dados)
+    continuar = input('Você quer cadastrar mais?\n1) SIM\n2) NÃO\n::>')
+    if continuar == '1':
+        print('Sim, quero cadastrar mais.')
+
+    elif continuar == '2':
+        print('Saindo do programa')
+        #Próxima linha é validação de cadastro. Pode retirar do programa Main.
+        print('Cadastrados',(hospedes))
+        break
