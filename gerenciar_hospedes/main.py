2d3f859a351abb8f2e5e3dda4c180830776b5a2f
JoaoCasali <joaovitorcasali@gmail.com>
2022-09-23 20:00:20 -0300

    main.py pronta

--- gerenciar_hospedes/main.py
+++ gerenciar_hospedes/main.py
@@ -10,7 +10,6 @@ print(f"{espaco}MENU{espaco}".center(50,"="))
 while True:
     print(f"(1) Fazer check-In\n(2) Relatório Hospedes\n(3) Procurar Hospedes\n(4) Fazer Check-Out\n(5) Finalizar Atendimento")
     print(poli)
-
     opcao = input("=> ")
     if not(opcao in funcoes):
         print("Digite uma opção válida.\n Uma das opções do MENU.")
@@ -18,49 +17,51 @@ while True:
     elif opcao == '1':
         varnome = input('Digite o seu nome: ').title()
         while True:
-            vartelefone = input('Digite o seu telefone: ')
+            vartelefone = input('Digite o seu telefone(ddxxxxxxxxx): ')
             if not(vartelefone.isnumeric()):
                 print('Isso não é numero!')
             else:
                 break
-        varcpf = input('Digite seu CPF: ')
+        while True:
+            varcpf = input('Digite seu CPF (xxx.xxx.xxx-xx): ')
 
-        hasError = False
-        # Limpa o cpf, retira pontos traços e tudo que não seja um número
-        cpf = [int(char) for char in varcpf if char.isdigit()]        
+            hasError = False
+            # Limpa o cpf, retira pontos traços e tudo que não seja um número
+            cpf = [int(char) for char in varcpf if char.isdigit()]        
 
-        # Verifica se o cpf são números repetidos
-        if ''.join(map(str, cpf)) in CPFS:
-            hasError = True
+            # Verifica se o cpf são números repetidos
+            if ''.join(map(str, cpf)) in CPFS:
+                hasError = True
 
-        # verifica se o tamanho está correto
-        if len(cpf) != 11:
-            hasError = True
+            # verifica se o tamanho está correto
+            if len(cpf) != 11:
+                hasError = True
 
-        if not(hasError):
-            for i in range(9, 11):
-                value = sum((cpf[num] * ((i+1) - num) for num in range(0, i)))
-                digit = ((value * 10) % 11) % 10
-                if digit != cpf[i]:
-                    hasError = True
-            
-        if hasError:
-            print(f"{poli}Deu merda{poli}")
-        else: 
-            print(f'{poli}Fim de cadastro{poli}')
-            dados = {
-                'nome': varnome, 
-                'cpf': varcpf,
-                'telefone': vartelefone
-            }
-            hospedes.append(dados)
+            if not(hasError):
+                for i in range(9, 11):
+                    value = sum((cpf[num] * ((i+1) - num) for num in range(0, i)))
+                    digit = ((value * 10) % 11) % 10
+                    if digit != cpf[i]:
+                        hasError = True
+                
+            if hasError:
+                print(f"{poli}CPF inválido!!{poli}")
+            else: 
+                print(f'{poli}Hospede cadastrado com sucesso!!{poli}')
+                dados = {
+                    'nome': varnome, 
+                    'cpf': varcpf,
+                    'telefone': vartelefone
+                }
+                hospedes.append(dados)
+                break
 
     elif opcao == '2':
         if len(hospedes) == 0:
             print(f"{poli}\n******************* NÃO HÁ HÓSPEDES ******************\n{poli}")
         else:
             for hospede in hospedes:
-                print("Nome: " + hospede["nome"] + " CPF: " + hospede["cpf"] + " Telefone: " + hospede["telefone"])
+                print("Nome: " + hospede["nome"] + " CPF: " + hospede["cpf"] + f" Telefone: ({hospede['telefone'][0:2]}) {hospede['telefone'][2:]}")
 
     elif opcao == '3':
         if len(hospedes) == 0:
@@ -72,7 +73,7 @@ while True:
             for hospede in hospedes:
                 if hospede["nome"] == nomeHospedeDigitado:
                     hospedeEncontrado = True
-                    print("Nome: " + hospede["nome"] + " CPF: " + hospede["cpf"] + " Telefone: " + hospede["telefone"])
+                    print("Nome: " + hospede["nome"] + " CPF: " + hospede["cpf"] + f" Telefone: ({hospede['telefone'][0:2]}) {hospede['telefone'][2:]}")
                     break
 
             if hospedeEncontrado == False:
--- gerenciar_hospedes/validacaoTelefone.py
+++
@@ -1,6 +0,0 @@
-telefone = input("Escreva um número: ")
-
-codArea = telefone[0:2]
-fone = telefone[2:]
-
-print(f'({codArea}) {fone}')
