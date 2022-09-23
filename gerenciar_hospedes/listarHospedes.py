a38e246d13172fd42d1aac7d29478f4732d0f526
JoaoCasali <joaovitorcasali@gmail.com>
2022-09-23 19:12:09 -0300

    Criando main.py

--- gerenciar_hospedes/listarHospedes.py
+++
@@ -1,6 +0,0 @@
-hospedes = [{"nome": 'Daniel', "cpf": '23232', "telefone": '123123'},
-            {"nome": 'Josimar', "cpf": '234242', "telefone": '6661623'},
-            {"nome": 'João', "cpf": '234242', "telefone": '6661623'}]
-
-for hospede in hospedes:
-    print("Nome: " + hospede["nome"] + " CPF: " + hospede["cpf"] + " Telefone: " + hospede["telefone"])
